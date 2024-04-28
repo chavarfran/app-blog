2344f8a5b7bff91723158f8f442151a778573de0
Francisco Chavarria <fchavarriar@outlook.com>
2024-04-28 00:46:58 -0600

    ✨ Feat: create file style and import

--- settings/settings.py
+++ settings/settings.py
@@ -1,4 +1,3 @@
-
 """
 Django settings for blog project.
 
@@ -10,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
